5cdb9f5b831ff1316012a838a9d524074fba0131
JessieJMC3 <71786129+JessieJMC3@users.noreply.github.com>
2020-10-27 21:02:15 +0000

    Add files via upload

---
+++ MODULE 5/script.js
@@ -0,0 +1,9 @@
+// DOM manupulation
+//console.log(document.getElementById(title));
+//console.log(document instanceof HTMLDocument);
+
+function sayHello () {
+    console.log(
+    document.getElementById("name").value
+    );
+}
